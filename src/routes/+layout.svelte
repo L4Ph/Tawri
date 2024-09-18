73042970aa0e609fb370dca52ad67549c1a9a5b1
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-09-19 00:31:10 +0900

    fix image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
   import { Toaster } from "$lib/components/ui/sonner";
   import '@fontsource-variable/m-plus-2';
   import { ModeWatcher } from "mode-watcher";
-  import { page } from '$app/stores';
+  import { page } from "$app/stores";
 </script>
 
+<svelte:head>
+  <meta property="og:image" content="{$page.url.origin}/og-image.jpg" />
+</svelte:head>
 <Toaster />
 <ModeWatcher />
 <slot />
