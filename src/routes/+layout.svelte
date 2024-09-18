d73f7c2dbcad28a02ec72cbbc12991cb294a3464
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-09-19 00:14:11 +0900

    fix og:image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,15 +9,3 @@
 <Toaster />
 <ModeWatcher />
 <slot />
-
-<svelte:head>
-  <meta property="og:title" content="Tawri - Web小説向けエディタ" />
-  <meta property="og:site_name" content="Tawri - Web小説向けエディタ" />
-  <meta property="og:description" content="速い、軽い、シームレスなWeb小説向けエディタ" />
-  <meta property="og:url" content="https://tawri.l4ph.moe" />
-  <meta property="og:image" content="{$page.url.origin}/og-image.jpg" />
-  <meta property="og:type" content="website" />
-  <meta name="twitter:site" content="@R0u9h" />
-  <meta name="twitter:creator" content="@R0u9h" />
-  <meta name="twitter:card" content="summary_large_image" />
-</svelte:head>
