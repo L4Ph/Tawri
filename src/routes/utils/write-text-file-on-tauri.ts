18f5ae516f38c02b1177d7946e8c172d1c931d7c
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-09-25 23:54:43 +0900

    add robots.txt etc...

--- src/routes/utils/write-text-file-on-tauri.ts
+++ src/routes/utils/write-text-file-on-tauri.ts
@@ -1,10 +1,17 @@
 import { writeTextFile } from "@tauri-apps/plugin-fs";
 
-export function writeTextFileOnTauri(
-	filePath: string | null,
-	_inputText: string,
-) {
+export async function writeTextFileOnTauri(
+	filePath: string,
+	inputText: string,
+): Promise<boolean> {
 	console.log("上書きするファイルのパス:", filePath);
-	console.log("上書きするテキスト:", _inputText);
-	// writeTextFile(filePath, inputText);
+	console.log("上書きするテキスト:", inputText);
+	try {
+		await writeTextFile(filePath, inputText);
+		console.log("ファイルの書き込みに成功しました。");
+		return true;
+	} catch (error) {
+		console.error("ファイルの書き込み中にエラーが発生しました:", error);
+		return false;
+	}
 }
