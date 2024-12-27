c19a9c3c778c4ef9a70e6f2a64a95338f9ffaa65
L4Ph <4ranci0ne@gmail.com>
2024-12-27 16:50:06 +0900

    add updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .setup(|app| {
---
+++ src/routes/utils/run-updater.ts
@@ -0,0 +1,32 @@
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+export async function runUpdater() {
+    const update = await check();
+    if (update) {
+        console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
+    let downloaded = 0;
+    let contentLength = 0;
+    // alternatively we could also call update.download() and update.install() separately
+    await update.downloadAndInstall((event) => {
+        switch (event.event) {
+        case 'Started':
+            if(typeof event.data.contentLength === "number") {
+                contentLength = event.data.contentLength;
+            }
+            console.log(`started downloading ${event.data.contentLength} bytes`);
+            break;
+        case 'Progress':
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+        case 'Finished':
+            console.log('download finished');
+            break;
+        }
+    });
+
+    console.log('update installed');
+    await relaunch();
+    }
+}
